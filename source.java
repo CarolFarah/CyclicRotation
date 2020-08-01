0225eda1159d23b81be7e3abd52e6ce916946c57
Ana Carolina Farah <anacarolfarah@gmail.com>
2020-08-01 02:26:59 -0300

    Update source.java

--- source.java
+++ source.java
@@ -1,18 +1,21 @@
 // you can also use imports, for example:
 // import java.util.*;
+
 // you can write to stdout for debugging purposes, e.g.
 // System.out.println("this is a debug message");
+
 class Solution {
-public int[] solution(int[] A, int K) {
-int[] rot = new int [A.length];
-for (int i = 0; i < A.length; i++) {
-if ( i + K >= A.length) {
-rot[(K + i) % A.length] = A[i];
-}
-else {
-rot [K + i] = A[i];
-}
-}
-return rot;
-}
+      public int[] solution(int[] A, int K) {
+          int[] rot = new int [A.length];
+        
+        for (int i = 0; i < A.length; i++) {
+            if ( i + K >= A.length) {
+                rot[(K + i) % A.length] = A[i];
+            }
+            else {
+                rot [K + i] = A[i];
+           }
+       }
+       return rot;
+    }
 }
